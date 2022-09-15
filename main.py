385c1c8d4c01f3b31e0e402ee5d818c2f4994242
Samuel Hegner <samuel.hegner@gmail.com>
2022-09-15 21:58:10 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+pygame.init()
+
+# Define some colors
+BLACK = ( 0, 0, 0)
+WHITE = ( 255, 255, 255)
+GREEN = ( 0, 255, 0)
+RED = ( 255, 0, 0)
+
+# Open a new window
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("My First Game")
+
+run = True
+
+clock = pygame.time.Clock()
+
+while run:
+    # --- Main event loop
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+              run = False # Flag that we are done so we can exit the while loop
+ 
+     # --- Game logic should go here
+ 
+     # --- Drawing code should go here
+     # First, clear the screen to white. 
+    screen.fill(WHITE)
+    #The you can draw different shapes and lines or add text to your background stage.
+    pygame.draw.rect(screen, RED, [55, 200, 100, 70],0)
+    pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
+    pygame.draw.ellipse(screen, BLACK, [20,20,250,100], 2)
+
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+    
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+print("Thanks for playing!")
+pygame.quit()
