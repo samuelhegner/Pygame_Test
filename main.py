cb3261cc3f65062cae889e750a6719182c49d7ba
Samuel Hegner <samuel.hegner@gmail.com>
2022-09-15 22:23:02 +0100

    drawing changes and formatting set up

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
 import pygame
+
 pygame.init()
 
 # Define some colors
-BLACK = ( 0, 0, 0)
-WHITE = ( 255, 255, 255)
-GREEN = ( 0, 255, 0)
-RED = ( 255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
 # Open a new window
-size = (700, 500)
+screen_height = 500
+screen_width = 700
+
+size = (screen_width, screen_height)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("My First Game")
 
-run = True
+run_game = True
 
 clock = pygame.time.Clock()
+draw = pygame.draw
 
-while run:
+while run_game:
     # --- Main event loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-              run = False # Flag that we are done so we can exit the while loop
- 
-     # --- Game logic should go here
- 
-     # --- Drawing code should go here
-     # First, clear the screen to white. 
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run_game = False  # Flag that we are done so we can exit the while loop
+
+    # --- Game logic should go here
+
+    # --- Drawing code should go here
     screen.fill(WHITE)
     #The you can draw different shapes and lines or add text to your background stage.
-    pygame.draw.rect(screen, RED, [55, 200, 100, 70],0)
-    pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-    pygame.draw.ellipse(screen, BLACK, [20,20,250,100], 2)
 
+    rect_width = screen_width / 6
+
+    for i in range(6):
+        draw.rect(screen, GREEN,
+                  [i * rect_width, 0, rect_width, screen_height], 0, 1)
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
-    
+
     # --- Limit to 60 frames per second
     clock.tick(60)
 
